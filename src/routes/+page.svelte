eb078fa80ae9a8e0e8d273f21bd093f4d162c135
hverlin <hugues.verlin@gmail.com>
2025-12-09 14:50:29 +0100

    test deploy

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,6 @@
+<script>
+</script>
+
 <div class="rounded-lg bg-brand-navy p-6 text-white shadow-lg">
 	<h1 class="text-2xl opacity-90">Carte des aménagements cyclables dans la Métropole de Lyon</h1>
 </div>
@@ -5,5 +8,6 @@
 <div style="view-transition-name: main-map;">
 	<div class="mt-10 mb-8 text-center text-2xl text-gray-600 hover:text-gray-800">
 		<a href="/carte">Voir la carte</a>
+		<a href="https://cyclopolis.fr">cyclopolis</a>
 	</div>
 </div>
